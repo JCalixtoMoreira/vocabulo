91a16cbe47c9c91c1682f7b2e4fea19d4a46834b
JCalixtoMoreira <joaocalixtomoreira@gmail.com>
2022-07-25 14:06:47 -0300

    adicionado novo endpoint e tratativa numero de palavras maximo

--- routes/palavraRoutes.js
+++ routes/palavraRoutes.js
@@ -4,11 +4,21 @@ const app = express();
 
 app.get("/palavras/:tamanho/:quantidade", async (request, response) => {
     try {
+        const maxTamanho = 20;
         const palavras = await palavraModel.aggregate([
-            { $match: { tamanho: { $eq: Number(request.params.tamanho) } } },
+            { $match: { tamanho: { $eq: Number(request.params.tamanho > maxTamanho ? maxTamanho : request.params.tamanho) } } },
             { $sample: { size: Number(request.params.quantidade) } }
         ]);
         response.send(palavras);
+    } catch (error) {
+        response.status(500).send(error);
+    }
+});
+
+app.get("/palavras/:conteudo", async (request, response) => {
+    try {
+        const palavra = await palavraModel.findOne({ conteudo: request.params.conteudo });
+        response.send(!!palavra);
     } catch (error) {
         response.status(500).send(error);
     }
